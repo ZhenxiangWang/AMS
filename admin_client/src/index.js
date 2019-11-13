fdd2569e3c7cc329da77f7284431b28be4b16fad
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-13 17:33:26 +1100

    basic structure

---
+++ admin_client/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+import App from "./App";
+
+// render App tag into div of index.html
+ReactDom.render(<App />, document.getElementById("root"));
