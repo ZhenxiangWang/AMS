b2738b104cd8d8ad15ee9f23a35cd78e8a7e151b
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-13 21:19:28 +1100

    import antd

--- admin_client/src/App.js
+++ admin_client/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
+import { Button } from "antd";
+import "./App.css";
 /*root component of the app*/
-
 export default class App extends Component {
   render() {
-    return <div>App2</div>;
+    return <Button type="primary">Primary</Button>;
   }
 }
