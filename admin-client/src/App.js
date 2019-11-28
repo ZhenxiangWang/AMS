5f99e4e6fdf161624d0da38e3dff4116d1c45c9d
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-11-28 17:58:06 +1100

    react-redux

--- admin-client/src/App.js
+++ admin-client/src/App.js
@@ -1,9 +1,23 @@
-import React, { Component } from "react";
-import { Button } from "antd";
-import "./App.css";
-/*root component of the app*/
+import React, {Component} from 'react'
+import {HashRouter, Route, Switch} from 'react-router-dom'
+
+import Login from './pages/login/login'
+import Admin from './pages/admin/admin'
+
+/*
+应用的根组件
+ */
 export default class App extends Component {
-  render() {
-    return <Button type="primary">Primary</Button>;
+
+
+  render () {
+    return (
+      <HashRouter>
+        <Switch> {/*只匹配其中一个*/}
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </HashRouter>
+    )
   }
 }
--- admin-client/src/index.js
+++ admin-client/src/index.js
@@ -1,6 +1,16 @@
-import React from "react";
-import ReactDom from "react-dom";
-import App from "./App";
+/*
+入口js
+ */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 
-// render App tag into div of index.html
-ReactDom.render(<App />, document.getElementById("root"));
+import store from './redux/store'
+import App from './App'
+
+// 将App组件标签渲染到index页面的div上
+ReactDOM.render((
+  <Provider store={store}>
+    <App />
+  </Provider>
+), document.getElementById('root'))
