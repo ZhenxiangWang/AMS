9f6770b8ea8d9cb62fa10c5dd6eb34543e961595
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-12-05 23:53:44 +1100

    advanced function and advanced component

--- admin-client/src/pages/login/login.jsx
+++ admin-client/src/pages/login/login.jsx
@@ -164,26 +164,29 @@ class Login extends Component {
 
 /*
 1. 高阶函数
-    1). 一类特别的函数
+    1). 一类特别的函数，有两种
         a. 接受函数类型的参数
         b. 返回值是函数
     2). 常见
-        a. 定时器: setTimeout()/setInterval()
-        b. Promise: Promise(() => {}) then(value => {}, reason => {})
+        a. 定时器: setTimeout()/setInterval()，接受函数类型的参数
+        b. Promise: Promise(() => {}) then(value => {}, reason => {}) ，接受函数类型的参数
         c. 数组遍历相关的方法: forEach()/filter()/map()/reduce()/find()/findIndex()
         d. 函数对象的bind()
         e. Form.create()() / getFieldDecorator()()
     3). 高阶函数更新动态, 更加具有扩展性
 
 2. 高阶组件
-    1). 本质就是一个函数
-    2). 接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
-    3). 作用: 扩展组件的功能
-    4). 高阶组件也是高阶函数: 接收一个组件函数, 返回是一个新的组件函数
+    1). 组件本质就是一个函数
+    2). 这个函数接收一个组件(被包装组件), 返回一个新的组件(包装组件), 包装组件会向被包装组件传入特定属性
+    3). 作用: 扩展组件的功能，实现更加强大，动态的功能
+    4). 高阶组件也是高阶函数: 接收一个组件(函数), 返回是一个新的组件(函数)
  */
 /*
+理解Form组件：包含<Form>的组件
 包装Form组件生成一个新的组件: Form(Login)
-新组件会向Form组件传递一个强大的对象属性: form
+新组件会向Form组件传递一个强大的对象属性: 属性名：form，属性值：对象
+此时Form是个对象，点的左边都是对象，括号的左边都是函数。
+Form.creat()返回的就是一个高阶组件。
  */
 const WrapLogin = Form.create()(Login);
 export default connect(state => ({ user: state.user }), { login })(WrapLogin);
