366b851870995a9a81ce7610aa2e658f0dbd661a
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-12-08 20:03:02 +1100

    add reducer

--- admin-client/src/redux/reducer.js
+++ admin-client/src/redux/reducer.js
@@ -1,47 +1,47 @@
 /*
 用来根据老的state和指定的action生成并返回新的state的函数
  */
-import {combineReducers} from 'redux'
+import { combineReducers } from "redux";
 
 /*
 用来管理头部标题的reducer函数
  */
-import storageUtils from "../utils/storageUtils"
+import storageUtils from "../utils/storageUtils";
 import {
   SET_HEAD_TITLE,
   RECEIVE_USER,
   SHOW_ERROR_MSG,
   RESET_USER
-} from './action-types'
+} from "./action-types";
 
-const initHeadTitle = ''
+const initHeadTitle = "";
 
 function headTitle(state = initHeadTitle, action) {
   switch (action.type) {
     case SET_HEAD_TITLE:
-      return action.data
+      return action.data;
     default:
-      return state
+      return state;
   }
 }
 
 /*
 用来管理当前登陆用户的reducer函数
  */
-const initUser = storageUtils.getUser()
+const initUser = storageUtils.getUser();
 
 function user(state = initUser, action) {
   switch (action.type) {
     case RECEIVE_USER:
-      return action.user
+      return action.user;
     case SHOW_ERROR_MSG:
-      const errorMsg = action.errorMsg
+      const errorMsg = action.errorMsg;
       // state.errorMsg = errorMsg  // 不要直接修改原本状态数据
-      return {...state, errorMsg}
+      return { ...state, errorMsg };
     case RESET_USER:
-      return {}
+      return {};
     default:
-      return state
+      return state;
   }
 }
 
@@ -56,4 +56,4 @@ function user(state = initUser, action) {
 export default combineReducers({
   headTitle,
   user
-})
+});
