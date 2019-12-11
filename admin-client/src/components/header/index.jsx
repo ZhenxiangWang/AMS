ba7f724438a25bf55ecf9841efa25a6ef192e674
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-12-11 21:52:08 +1100

    welcome

--- admin-client/src/components/header/index.jsx
+++ admin-client/src/components/header/index.jsx
@@ -107,8 +107,8 @@ class Header extends Component {
     return (
       <div className="header">
         <div className="header-top">
-          <span>欢迎, {username}</span>
-          <LinkButton onClick={this.logout}>退出</LinkButton>
+          <span>WELCOME, {username}</span>
+          <LinkButton onClick={this.logout}>LOGOUT</LinkButton>
         </div>
         <div className="header-bottom">
           <div className="header-bottom-left">{title}</div>
