bcd04de767c24291d4138aa4d07f168815ab86c5
ZhenxiangWang <wangzhenxiang2016@outlook.com>
2019-12-16 20:54:40 +1100

    order

--- admin-client/src/pages/order/order.jsx
+++ admin-client/src/pages/order/order.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './order.less'
+import React from "react";
+import "./order.less";
 
 /*
 Home路由组件
@@ -7,7 +7,7 @@ Home路由组件
 export default function Order(props) {
   return (
     <div className="order">
-      订单管理模块学生实战开发中...
+      The order management module is under development ...
     </div>
-  )
+  );
 }
